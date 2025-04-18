2d8cc024e70528f8f340db7e035de473f5646955
Hamza Hammouch <hamzapc212@gmail.com>
2025-04-18 14:10:40 +0100

    🍄 get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -9,31 +9,18 @@
 
 char *get_next_line(int fd)
 {
-	if (fd < 0 || BUFFER_SIZE < 1 )
-		return 0;
-
-	char *str  = malloc (100000);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return NULL;
+	char *str = malloc(100000);
 	char *temp = str;
 
-	if (BUFFER_SIZE == 1)
-	{
-		while(read(fd, temp, BUFFER_SIZE) > 0 && *temp++ != '\n');
-		if(temp > str)
-			return (*temp = 0,str);
-		else
-			return (free(str),NULL);
-	}
+	while(read(fd, temp, 1) > 0 && *temp++ != '\n');
+	if (temp > str)
+		return (*temp = 0, str);
 	else
-	{
-		while(read(fd, temp, 1) > 0 && *temp++ != '\n');
-		
-		if(temp > str)
-			return (*temp = 0,str);
-		else
-			return (free(str),NULL);
-	}
-    return(str);
+		return (free(str), NULL);
 }
+
 // int main ()
 // {
 // 	int fd = open("file.txt", O_RDWR);
